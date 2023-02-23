a9950d520b535a34fb5c205f85faed36d2cb1a2e
aleksmarsha <27923826+aleksmarsha@users.noreply.github.com>
2023-02-23 06:02:16 +0200

    Add a function for pick a color and change another for work with input range

--- script.js
+++ script.js
@@ -2,13 +2,24 @@ const rowsAndColumns = +document.getElementById("modify").value;
 
 function fillPaper(value) {
 	const whitePaper = document.getElementById("paper");
+	whitePaper.innerHTML = "";
 	whitePaper.style.gridTemplateColumns = `repeat(${value}, auto)`;
-
 	for(let i = 1; i <= value * value; i++) {
 		const div = document.createElement("div");
 		div.id = `section${i}`;
+		div.classList.add("sections");
 		whitePaper.append(div);
 	}
 }
 
 fillPaper(rowsAndColumns);
+
+let color = "black";
+const choice = Array.from(document.getElementsByClassName("paint"));
+choice.forEach(clr => {
+	let idColor = clr.getAttribute("id");
+	clr.addEventListener('click', () => {
+		color = idColor;
+		return color;
+	});
+});
